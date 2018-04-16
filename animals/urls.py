d79262cc2ebb99c5ac20e07f7b7a29bd1689961c
Holger Dinkel <holger.dinkel@leibniz-fli.de>
2018-04-16 13:55:05 +0200

    add urls.py

--- animals/urls.py
+++ animals/urls.py
@@ -1,4 +1,4 @@
-"""animatch URL Configuration
+"""animals URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
 
-from animals import urls
+from django.urls import path
+from . import views
 
+app_name = 'animals'
 urlpatterns = [
-    path('animals/', include('animals.urls', namespace='animals-index')),
-    path('admin/', admin.site.urls),
+    path('index', views.AnimalIndexView.as_view(), name='index'),
 ]
