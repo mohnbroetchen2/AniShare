e5e88dafe0b83e4b2f3ebe2c3b16b531f1e967b8
Holger Dinkel <holger.dinkel@leibniz-fli.de>
2018-04-16 13:32:58 +0200

    update admin.py

--- animals/admin.py
+++ animals/admin.py
@@ -4,15 +4,20 @@ from .models import Animal, Person, Lab
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    empty_value_display = '-empty-'
     list_display = ('name', 'email','responsible_for_lab')
     search_fields=('name','email','responsible_for_lab__name')
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
-    list_display = ( 'amount', 'entry_date', 'age', 'pyrat_id', 'pyrat_lab_id', 'day_of_birth', 'line', 'sex', 'location', 'responsible_person', 'available_from', 'available_to')
+    list_display = ( 'amount', 'entry_date', 'day_of_birth', 'age', 'available_from', 'available_to', 'line', 'sex', 'location', 'new_owner')
+    list_display_links = ( 'amount', 'entry_date', 'day_of_birth', 'age', 'available_from', 'available_to', 'line', 'sex', 'location', 'new_owner')
+    search_fields = ( 'amount', 'entry_date', 'external_id', 'external_lab_id', 'day_of_birth', 'line', 'sex', 'location', 'responsible_person__name', 'new_owner', 'mutations', 'available_from', 'available_to')
     autocomplete_fields = ['responsible_person']
     list_filter = ('sex', 'responsible_person__responsible_for_lab', 'location')
-#    readonly_fields = ('age',)
+    radio_fields = {'sex':admin.HORIZONTAL}
+    readonly_fields = ('creation_date','modification_date')
 
-admin.site.register(Lab)
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ('name','responsible_person')
+    search_fields=('name',)
