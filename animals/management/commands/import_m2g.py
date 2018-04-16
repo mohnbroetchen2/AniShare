558ae635b4e9ad551b4f8f4ab5ec2460fe98702d
Holger Dinkel <holger.dinkel@leibniz-fli.de>
2018-04-16 09:38:25 +0200

    finish import script to import all entries

--- animals/management/commands/import_m2g.py
+++ animals/management/commands/import_m2g.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 from animals.models import Animal, Person, Lab
 import datetime
 import xlrd
@@ -28,32 +29,34 @@ class Command(BaseCommand):
 
             a.pk = int(xl_sheet.cell(row_idx, 0).value)
             a.entry_date = xldate_as_datetime(xl_sheet.cell(row_idx, 1).value, xl.datemode)
-            a.pyrat_id = str(xl_sheet.cell(row_idx, 2).value)
-            a.lab_id = str(xl_sheet.cell(row_idx, 3).value)
+            a.pyrat_id = str(xl_sheet.cell(row_idx, 2).value).strip()
+            a.lab_id = str(xl_sheet.cell(row_idx, 3).value).strip()
             a.day_of_birth = xldate_as_datetime(xl_sheet.cell(row_idx, 4).value, xl.datemode)
             age = int(xl_sheet.cell(row_idx, 5).value)
-            a.line = str(xl_sheet.cell(row_idx, 6).value)
-            mutation_1 = str(xl_sheet.cell(row_idx, 7).value)
-            grade_1 = str(xl_sheet.cell(row_idx, 8).value)
-            mutation_2 = str(xl_sheet.cell(row_idx, 9).value)
-            grade_2 = str(xl_sheet.cell(row_idx, 10).value)
-            mutation_3 = str(xl_sheet.cell(row_idx, 11).value)
-            grade_3 = str(xl_sheet.cell(row_idx, 12).value)
-            mutation_4 = str(xl_sheet.cell(row_idx, 13).value)
-            grade_4 = str(xl_sheet.cell(row_idx, 14).value)
-            a.sex = str(xl_sheet.cell(row_idx, 15).value)
-            a.location = str(xl_sheet.cell(row_idx, 16).value)
-            a.licence_number = str(xl_sheet.cell(row_idx, 17).value)
-            responsible_person = str(xl_sheet.cell(row_idx, 18).value)
+            a.line = str(xl_sheet.cell(row_idx, 6).value).strip()
+            mutation_1 = str(xl_sheet.cell(row_idx, 7).value).strip()
+            grade_1 = str(xl_sheet.cell(row_idx, 8).value).strip()
+            mutation_2 = str(xl_sheet.cell(row_idx, 9).value).strip()
+            grade_2 = str(xl_sheet.cell(row_idx, 10).value).strip()
+            mutation_3 = str(xl_sheet.cell(row_idx, 11).value).strip()
+            grade_3 = str(xl_sheet.cell(row_idx, 12).value).strip()
+            mutation_4 = str(xl_sheet.cell(row_idx, 13).value).strip()
+            grade_4 = str(xl_sheet.cell(row_idx, 14).value).strip()
+            a.sex = str(xl_sheet.cell(row_idx, 15).value).strip()
+            a.location = str(xl_sheet.cell(row_idx, 16).value).strip()
+            a.licence_number = str(xl_sheet.cell(row_idx, 17).value).strip()
+            responsible_person = str(xl_sheet.cell(row_idx, 18).value).strip().strip()
             a.available_from = xldate_as_datetime(xl_sheet.cell(row_idx, 19).value, xl.datemode)
             a.available_to = xldate_as_datetime(xl_sheet.cell(row_idx, 20).value, xl.datemode)
-            a.new_owner = str(xl_sheet.cell(row_idx, 21).value)
-            a.responsible_person = Person.objects.get(id=1)
+            a.new_owner = str(xl_sheet.cell(row_idx, 21).value).strip()
 
-            a.mutation =  mutation_1.strip() + ' ' + grade_1.strip() + '\n'
-            a.mutation += mutation_2.strip() + ' ' + grade_2.strip() + '\n'
-            a.mutation += mutation_3.strip() + ' ' + grade_3.strip() + '\n'
-            a.mutation += mutation_4.strip() + ' ' + grade_4.strip() + '\n'
+            a.mutation =  mutation_1 + ' ' + grade_1 + '\n'
+            a.mutation += mutation_2 + ' ' + grade_2 + '\n'
+            a.mutation += mutation_3 + ' ' + grade_3 + '\n'
+            a.mutation += mutation_4 + ' ' + grade_4 + '\n'
+
+
+            a.responsible_person = Person.objects.get(Q(email=responsible_person.lower()) | Q(name__iexact=responsible_person.lower()))
             a.save()
 
 
