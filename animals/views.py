ed9dc87f7e4830c8faea903d48436cb2f5f350e8
Holger Dinkel <holger.dinkel@leibniz-fli.de>
2018-04-16 13:32:41 +0200

    add template and views

--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Animal, Person
+
+
+class AnimalIndexView(generic.ListView):
+    template_name = 'animals/index.html'
+    context_object_name = 'all_animals'
+    def get_queryset(self):
+        """Return the latest additions to the Animals table"""
+        return Animal.objects.order_by('-entry_date')#[:100]
