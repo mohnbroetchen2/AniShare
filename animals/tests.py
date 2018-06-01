7b69648b710140a80fbc8b58fef4a4077a63af59
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2018-06-01 09:53:49 +0200

    rewrite tests to check that login was provided

--- animals/tests.py
+++ animals/tests.py
@@ -18,18 +18,24 @@ class GetAnimalsTest(TestCase):
 
     def test_get_all_animals(self):
         """ try to retrieve all animals """
+        response = self.client.get('/animals/')
+        self.assertEqual(response.status_code, 302)
         self.client.login(username='testuser', password='12345')
         response = self.client.get('/animals/')
         self.assertEqual(response.status_code, 200)
 
     def test_get_one_animal(self):
         """ try to retrieve individual animals """
+        response = self.client.get('/animals/1')
+        self.assertEqual(response.status_code, 302)
         self.client.login(username='testuser', password='12345')
         response = self.client.get('/animals/1')
         self.assertEqual(response.status_code, 200)
 
     def test_claim_one_animal(self):
         """ try to claim individual animals """
+        response = self.client.get('/animals/claim/1')
+        self.assertEqual(response.status_code, 302)
         self.client.login(username='testuser', password='12345')
         response = self.client.get('/animals/claim/1')
         self.assertEqual(response.status_code, 200)
