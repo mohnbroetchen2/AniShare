7a423340ebcd064e8efd86dcde2e32b5a8ef5d61
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2018-06-05 17:09:05 +0200

    filters: use 'icontains' for mutations and line

--- animals/filters.py
+++ animals/filters.py
@@ -1,11 +1,14 @@
 """
 Filters: using django-filters to filter querysets
 """
+import django_filters
 from django_filters import FilterSet
 from .models import Animal
 
 class AnimalFilter(FilterSet):
+    mutations = django_filters.CharFilter(lookup_expr='icontains')
+    line = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Animal
-        fields = ['sex', 'animal_type', 'day_of_birth', 'line', 'location', 'responsible_person',
-                  'added_by' ]
+        fields = ['animal_type', 'sex', 'day_of_birth', 'line', 'mutations', 'location',
+                  'responsible_person', 'added_by' ]
