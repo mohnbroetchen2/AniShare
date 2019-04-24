d8df788060dcd7338d01675b6721f7dfb0d7bce6
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2019-04-24 11:31:15 +0200

    add uml model and bugfix jobs

--- animals/jobs/daily/hourly_check_status_incidents_testsystem.py
+++ animals/jobs/daily/hourly_check_status_incidents_testsystem.py
@@ -6,13 +6,13 @@ class Job(DailyJob):
 
     def execute(self):
         from django.core import management
-        from ...models import WIncident, WIncidentAnimals, Animal, Mouse, WIncidentcomment
+        from ...models import WIncident, WIncidentAnimals, Animal, Mouse, WIncidentcomment, WIncidentPups, Pup
         from django.core.mail import EmailMultiAlternatives, send_mail
         from datetime import datetime, timedelta
         from django.conf import settings
         import logging
 
-        mousedb = 'mousedb_test'
+        mousedb = 'mousedb'
         logger = logging.getLogger('myscriptlogger')
         try:
             today = datetime.now().date()
@@ -22,27 +22,48 @@ class Job(DailyJob):
                 error = 0
                 i = 0
                 animallist = WIncidentAnimals.objects.using(mousedb).filter(incidentid = incident.incidentid)
-                count_animals = animallist.count()
+                puplist = WIncidentPups.objects.using(mousedb).filter(incidentid = incident.incidentid)
+                count_mice = animallist.count()
+                count_pups = puplist.count()
+                count_animals = count_mice + count_pups
                 for pyratmouse in animallist:
                     i = i + 1
                     try:
                         animouse = Animal.objects.get(mouse_id=pyratmouse.animalid)
-                        if (animouse.available_to >= today):
+                        if (animouse.new_owner):
+                            continue
+                        if (animouse.available_to >= today:
                             skip = 1
                             break
                     except Exception: 
                             error = 1
                             skip = 1
                             ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
-                            send_mail("AniShare Check Status Error", 'Fehler {} bei der Statusüberprüfung des Auftrags {}'.format( Exception, incident.incidentid), ADMIN_EMAIL, [ADMIN_EMAIL])
-                    if (skip == 0 and i == count_animals):
-                        incident.status = 1
-                        incident.save()
-                        logger.debug('{}: Incident status {} has been changed to 1.'.format(datetime.now(), incident.incidentid))
-                        new_comment = WIncidentcomment()
-                        new_comment.incidentid = incident
-                        new_comment.comment = 'AniShare: Request status changed to closed'
-                        new_comment.save() 
+                            send_mail("AniShare Check Status Error", 'Fehler {} bei der Statusüberprüfung des Auftrags {} (Maus)'.format( Exception, incident.incidentid), ADMIN_EMAIL, [ADMIN_EMAIL])
+                
+                for pyratpup in puplist
+                    i = i + 1
+                    try:
+                        anipup = Animal.objects.get(pup_id=pyratpup.pupid)
+                        if (anipup.new_owner):
+                            continue
+                        if (anipup.available_to >= today:
+                            skip = 1
+                            break
+                    except Exception: 
+                            error = 1
+                            skip = 1
+                            ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
+                            send_mail("AniShare Check Status Error", 'Fehler {} bei der Statusüberprüfung des Auftrags {} (Pup)'.format( Exception, incident.incidentid), ADMIN_EMAIL, [ADMIN_EMAIL])
+
+                if (skip == 0 and i == count_animals):
+                    incident.status = 1
+                    incident.save()
+                    logger.debug('{}: Incident status {} has been changed to 1.'.format(datetime.now(), incident.incidentid))
+                    new_comment = WIncidentcomment()
+                    new_comment.incidentid = incident
+                    new_comment.comment = 'AniShare: Request status changed to closed'
+                    new_comment.save() 
         except Exception: 
             management.call_command("clearsessions")
             ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
