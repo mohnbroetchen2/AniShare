5efd16893b8958c29327d31c8f253be10f11385b
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2019-04-25 10:23:19 +0200

    changed time settings

--- anishare/settings.py
+++ anishare/settings.py
@@ -161,7 +161,7 @@ DATETIME_FORMAT = 'd.m.Y H:i:sO'
 DATE_FORMAT = 'd.m.Y'
 DATE_INPUT_FORMATS =\
 [
-    '%d.%m.%y', '%d.%m.%Y',
+    '%d.%m.%Y', '%d.%m.%Y',
     '%Y-%m-%d', '%m/%d/%Y', '%m/%d/%y', # '2006-10-25', '10/25/2006', '10/25/06'
     '%b %d %Y', '%b %d, %Y',            # 'Oct 25 2006', 'Oct 25, 2006'
     '%d %b %Y', '%d %b, %Y',            # '25 Oct 2006', '25 Oct, 2006'
