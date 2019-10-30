e1ae5ad1d979ff57061a9c897f573041239d4b1f
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2019-10-30 11:24:56 +0100

    bugfix

--- animals/jobs/hourly/hourly_insert_from_pyrat_testsystem.py
+++ animals/jobs/hourly/hourly_insert_from_pyrat_testsystem.py
@@ -145,10 +145,10 @@ class Job(HourlyJob):
                             new_comment.incidentid = incident
                             if dataset.eartag:
                                 new_comment.comment = 'Pup {} without licence can not be imported'.format(dataset.eartag)
-                                send_mail("AniShare: Pup without license", 'You created a work request with the ID {} to add the pup {} to AniShare. It is not possible to import a pup without a license. '.format(incident.id, dataset.eartag), ADMIN_EMAIL, [initiator_mail,ADMIN_EMAIL])
+                                send_mail("AniShare: Pup without license", 'You created a work request with the ID {} to add the pup {} to AniShare. It is not possible to import a pup without a license. '.format(incident.incidentid, dataset.eartag), ADMIN_EMAIL, [initiator_mail,ADMIN_EMAIL])
                             else:
                                 new_comment.comment = 'Pup {} without licence can not be imported'.format(pyratpup.pupid)
-                                send_mail("AniShare: Pup without license", 'You created a work request with the ID {} to add the pup {} to AniShare. It is not possible to import a pup without a license. '.format(incident.id, dataset.id), ADMIN_EMAIL, [initiator_mail,ADMIN_EMAIL])
+                                send_mail("AniShare: Pup without license", 'You created a work request with the ID {} to add the pup {} to AniShare. It is not possible to import a pup without a license. '.format(incident.incidentid, dataset.id), ADMIN_EMAIL, [initiator_mail,ADMIN_EMAIL])
                             new_comment.save(using=mousedb_write)
                             new_comment.commentdate = new_comment.commentdate + timedelta(hours=TIMEDIFF)
                             new_comment.save(using=mousedb_write)
