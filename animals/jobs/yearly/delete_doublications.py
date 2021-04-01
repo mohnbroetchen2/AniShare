ba263e34f86b20fe58b66749d1140a1a4b102785
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-04-01 12:50:10 +0200

    bugfix

--- animals/jobs/yearly/delete_doublications.py
+++ animals/jobs/yearly/delete_doublications.py
@@ -18,18 +18,26 @@ class Job(YearlyJob):
 
         ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
 
-        ids = range(16075,26053)
+        #ids = range(16075,26053)
         #ids = range(7100,7143)
         try:
             animals = Animal.objects.all()
+            dismiss_animals = []
             for a in animals:
+                if a in dismiss_animals:
+                    continue
                 try:
-                    douples = Animal.objects.filter(database_id=a.database_id)
-                    for douple in douples:
-                        if douple.pk == a.pk:
+                    doubles = Animal.objects.filter(database_id=a.database_id)
+                    for double in doubles:
+                        if double.id == a.id:
+                            dismiss_animals.append(double)
                             continue
                         else:
-                            douple.delete()
+                            if double in dismiss_animals:
+                                continue
+                            else:
+                                dismiss_animals.append(double)
+                                double.delete()
                 except:
                     continue
             #animals = Animal.objects.all()
@@ -39,5 +47,5 @@ class Job(YearlyJob):
         except BaseException as e: 
             management.call_command("clearsessions")
             ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
-            send_mail("AniShare Importscriptfehler delete_douples", ': Fehler {} in Zeile {}'.format( e,sys.exc_info()[2].tb_lineno), ADMIN_EMAIL, [ADMIN_EMAIL])
+            send_mail("AniShare Importscriptfehler delete_doubles", ': Fehler {} in Zeile {}'.format( e,sys.exc_info()[2].tb_lineno), ADMIN_EMAIL, [ADMIN_EMAIL])
         management.call_command("clearsessions")
