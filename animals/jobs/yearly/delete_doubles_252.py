6e82f83a97b308576a62a1e49be7b5d4c155a0c9
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-06-18 08:59:01 +0200

    add delete_doubles_252.py

---
+++ animals/jobs/yearly/delete_doubles_252.py
@@ -0,0 +1,44 @@
+from django_extensions.management.jobs import YearlyJob
+
+
+class Job(YearlyJob):
+    help = ""
+
+    def execute(self):
+        from django.core import management
+        from ...models import WIncident, WIncident_write, WIncidentAnimals, Animal, Mouse, Pup, MouseMutation, Location, Person, Lab, WIncidentcomment, WIncidentPups
+        from django.contrib.auth.models import User
+        from django.core.mail import EmailMultiAlternatives, send_mail
+        from datetime import datetime, timedelta
+        from django.conf import settings
+        import logging
+        import sys
+
+        logger = logging.getLogger('myscriptlogger')
+
+        ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
+
+        #ids = range(16075,26053)
+        #ids = range(7100,7143)
+        try:
+            animals = Animal.objects.filter(entry_date__range=["2021-02-25", "2021-02-25"])
+            dismiss_animals = []
+            for a in animals:
+                if a in dismiss_animals:
+                    continue
+                try:
+                    original = Animal.objects.filter(database_id=a.database_id).exlude(entry_date=datetime.date(2021,2,25))
+                    if len(original) == 1:
+                        logger.debug('Doppelter Eintrag von {}'.fromat(a.database_id))
+                        #a.delete()
+                except:
+                    continue
+            #animals = Animal.objects.all()
+            #for a in animals:
+            #    if a.pk in ids:
+            #        a.delete() 
+        except BaseException as e: 
+            management.call_command("clearsessions")
+            ADMIN_EMAIL = getattr(settings, "ADMIN_EMAIL", None)
+            send_mail("AniShare Importscriptfehler delete_doubles", ': Fehler {} in Zeile {}'.format( e,sys.exc_info()[2].tb_lineno), ADMIN_EMAIL, [ADMIN_EMAIL])
+        management.call_command("clearsessions")
