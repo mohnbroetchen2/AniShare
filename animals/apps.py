84b02e330badbdca3864d68c5e904302d27e1ec3
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-06-23 10:21:53 +0200

    remove receiver(post_save, sender=Organ)

--- animals/apps.py
+++ animals/apps.py
@@ -4,8 +4,8 @@ from django.apps import AppConfig
 class AnimalsConfig(AppConfig):
     name = 'animals'
 
-@receiver(post_save, sender=Organ)
-def update_killing_person(sender, instance, created, **kwargs):
-    if (instance.killing_person == None):
-        instance.killing_person = instance.responsible_person__email
-        instance.killing_person.save()
+#@receiver(post_save, sender=Organ)
+#def update_killing_person(sender, instance, created, **kwargs):
+#    if (instance.killing_person == None):
+#        instance.killing_person = instance.responsible_person__email
+#        instance.killing_person.save()
