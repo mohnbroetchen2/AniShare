68b9967835ac6077b0cf407efd738d3361e448b2
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-12-14 15:49:20 +0100

    remove old function

--- anishare/settings.py
+++ anishare/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'bootstrap_email',
     'django_extensions', # for jobs scheduling
     'simple_history',
-    'pyrat_api',
     'adminrestrict',
     'defender',
     'crispy_forms',
