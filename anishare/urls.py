2698cfdeea1e12fa871550ae122820224104cdb5
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-12-22 13:11:17 +0100

    removed unused bib

--- anishare/urls.py
+++ anishare/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.conf.urls import url
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 from django.views.decorators.cache import never_cache
 
 from animals import urls
