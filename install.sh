28dee9e7ca2967e7c9c3a5666e96298c8edd9cba
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2022-02-08 16:31:38 +0100

    new install commands

--- install.sh
+++ install.sh
@@ -6,19 +6,26 @@ group=www-data			# www-data is the apache group. you don't need to edit this
 
 
 sudo apt-get update
-sudo apt-get install git python3 python3-dev python3-ldap python3-pip default-libmysqlclient-dev mysql-client ldap-utils libldap2-dev libsasl2-dev apache2 redis -y
+sudo apt-get install git python3 python3-dev python3-ldap python3-pip default-libmysqlclient-dev mysql-client ldap-utils libldap2-dev libsasl2-dev apache2 redis pipenv libapache2-mod-wsgi-py3 -y
 cd /var/www/
 git clone https://github.com/mohnbroetchen2/AniShare.git
 cd AniShare
-pipenv shell
+virtualenv ./
 pip install -r requirements.txt
-cp anishare/local_settings.py.template anishare/local_settings.py
-sed -e 's/%SECRET_KEY%/$secret_key' \ 
-    -e 's/%EMAIL_HOST%/$host'< anishare/local_settings.py.template > anishare/local_settings.py
+sed -e 's/%SECRET_KEY%/'$secret_key'/' \ 
+    -e 's/%EMAIL_HOST%/'$host'/'< anishare/local_settings.py.template > anishare/local_settings.py
 python manage.py collectstatic
 python manage.py migrate
-sudo cp -a /etc/redis/default.conf.example /etc/redis/anishare.conf
-sudo chown root:redis  /etc/redis/anishare.conf
-sudo chmod u=rw,g=r,o=  /etc/redis/anishare.conf
-sudo install -d -o redis -g redis -m 0750 /var/lib/redis/anishare/
-systemctl start redis@anishare
+chmod 664 /var/www/AniShare/db.sqlite3
+cd /var/www/
+chown $user:$group AniShare -R
+rm /etc/apache2/sites-enabled/000-default.conf
+cp /var/www/AniShare/apache_conf.template /etc/apache2/sites-available/anishare.conf
+ln -s /etc/apache2/sites-available/anishare.conf /etc/apache2/sites-enabled/anishare.conf
+cp /etc/apache2/sites-enabled/
+service apache2 restart
+#sudo cp -a /etc/redis/default.conf.example /etc/redis/anishare.conf
+#sudo chown root:redis  /etc/redis/anishare.conf
+#sudo chmod u=rw,g=r,o=  /etc/redis/anishare.conf
+#sudo install -d -o redis -g redis -m 0750 /var/lib/redis/anishare/
+#systemctl start redis@anishare
